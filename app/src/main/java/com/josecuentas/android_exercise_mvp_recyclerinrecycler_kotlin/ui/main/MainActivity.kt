729d8aed0f7ab0a6618fdb855b65e26ff888386b
José Cuentas Turpo <jcuentast@gmail.com>
2017-09-20 16:49:31 -0500

    feat(design): main:  - design basic main  - implement CoordinatorLayout  - add theme NoActionBar

---
+++ app/src/main/java/com/josecuentas/android_exercise_mvp_recyclerinrecycler_kotlin/ui/main/MainActivity.kt
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2017, José Norberto Cuentas Turpo.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.josecuentas.android_exercise_mvp_recyclerinrecycler_kotlin.ui.main
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import com.josecuentas.android_exercise_mvp_recyclerinrecycler_kotlin.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        rviContainer.layoutManager = LinearLayoutManager(this)
+        rviContainer.setHasFixedSize(false)
+        rviContainer.adapter = object : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+            override fun getItemCount(): Int = 30
+
+            override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+                (holder.itemView as TextView).text = position.toString()
+            }
+
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+                val view = LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false)
+                return object : RecyclerView.ViewHolder(view) {}
+            }
+
+        }
+    }
+}
